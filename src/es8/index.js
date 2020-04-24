636af77d44ad5ca224b0742aa0bf4099d9879ed4
JefferLindao <jefferlindao@gmail.com>
2020-04-24 09:48:34 -0500

    Clase6 - es8 Objeto uso de padStart padEnd

---
+++ src/es8/index.js
@@ -0,0 +1,28 @@
+// es8
+const data = {
+    frontend: 'Jefferson',
+    backend: 'Isabel',
+    design: 'Ana'
+}
+
+const entries = Object.entries(data)
+console.log(entries)
+console.log(entries.length)
+
+const data = {
+    frontend: 'Jefferson',
+    backend: 'Isabel',
+    design: 'Ana'
+}
+const values = Object.values(data)
+console.log(values)
+
+
+const string = 'hello'
+console.log(string.padStart(7, 'hi'))
+console.log(string.padEnd(12, ' World'))
+
+const obj = {
+    name: 'oscar',
+}
+console.log(obj)
