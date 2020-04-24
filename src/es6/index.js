bf3ed4ff138ec40b9e5f0f43659da32159d1688c
JefferLindao <jefferlindao@gmail.com>
2020-04-23 23:02:52 -0500

    clase2 - let y const, Multilinea, Spread Operator y Desestructura

--- src/es6/index.js
+++ src/es6/index.js
@@ -18,3 +18,38 @@ let concadenacion = hello + ' ' + word
 
 let concadenacion1 = `${hello} ${word}`
 console.log(concadenacion1)
+
+//let y const, Multilinea, Spread Operator y Desestructura
+
+let lorem = "lorem noinsjf jndkn \n" +
+    "otra frase"
+let lorem2 = `otra frase epica,
+pues es otra`
+console.log(lorem)
+console.log(lorem2)
+
+// desestructuracion
+let person = {
+    name: 'Jefferson',
+    age: 27,
+    country: 'EC'
+}
+console.log(person.name, person.age, person.country)
+let { name, age, country } = person
+console.log(name, age, country)
+
+// Spread Operator
+let team1 = ['Oscar', 'Jefferson', 'Leon']
+let team2 = ['Valeria', 'Maria', 'Fiorella']
+let education = ['David', ...team1, ...team2]
+console.log(education)
+
+{
+    var globalVar = 'variable global'
+}
+
+{
+    let global1 = 'letGlobal'
+    console.log(global1)
+}
+console.log(globalVar)
