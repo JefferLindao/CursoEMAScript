1490c75db48302598f8a2a9048229a64ff416112
JefferLindao <jefferlindao@gmail.com>
2020-04-24 08:09:57 -0500

    Clase5 - ES8 include expotencial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// EMAScript 7
+// include
+let numbers = [1, 2, 3, 9, 5, 4]
+if (numbers.includes(6)) {
+    console.log(`Si se encuentra el valor 7`)
+} else {
+    console.log(`No se encuentra `)
+}
+
+// expotencial
+let base = 4
+let exponent = 35
+let result = base ** exponent
+
+console.log(result)
